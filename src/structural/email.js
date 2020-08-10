517bc621bbb3ba81c9be287b3c2e4553a29be34c
yahm23 <yahmed1996a@gmail.com>
2020-08-10 11:25:51 +0100

    done front end entirety, backend not set up

--- src/structural/email.js
+++ src/structural/email.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Email() {
+
+    const[email,setEmail] =useState('');
+
     return (
-        <div>
+        <div className="formContainer">
+            <form className="form">
+
+                <input onChange={()=>setEmail()} className="inputEmail" defaultValue="Email Address" value={email}></input>
+                <button className="buttonEmail" >Get Notified</button>
+            </form>
         </div>
     )
 }
