256d1d985576be2118b8b0fde14869cff64aff95
yahm23 <yahmed1996a@gmail.com>
2020-08-10 13:22:07 +0100

    proportion is constant inside form, uses width rather than height

--- src/structural/email.js
+++ src/structural/email.js
@@ -3,6 +3,8 @@ import React, {useState} from 'react'
 export default function Email() {
 
     const[email,setEmail] = useState('');
+    
+    // const sheetsAPI = 'https://sheetsu.com/apis/v1.0bu/0d1354db0628'
 
     const emailSetter = (event) => {
         console.log(event.target.value);
@@ -10,9 +12,26 @@ export default function Email() {
 
     }
 
+    const handleSubmit= (event) => {
+        event.preventDefault();
+    
+        // fetch(sheetsAPI, {
+        //   headers: {
+        //     'Accept': 'application/json',
+        //     'Content-Type': 'application/json'
+        //   },
+        //   method: "POST",
+        //   body: JSON.stringify(email)
+        //  }).then( (response) => {
+        //     return response.json()
+        //   }).then( (json) => {
+        //     console.log(json);
+        //   });
+      }
+
     return (
         <div className="formContainer">
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
 
                 <input 
                     onChange={emailSetter}
