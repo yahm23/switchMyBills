00d2d79ab9544d4b2abef16e8d0ee1165be124b7
yahm23 <yahmed1996a@gmail.com>
2020-08-10 10:50:54 +0100

    another font added, need to make email component now

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Email from '../structural/email'
-
+import Logo from '../media/logoLight.png'
 export default function Main() {
     return (
-        <div>
-            <h1 className="mainBanner" > Switch your energy for the last time. </h1>
-            <Email></Email>
+        <div className="outerMain">
+            <div className = "mainBanner">
+                <img className = "logoMain" src={Logo}/>
+                <h1 className="mainBannerText" > Switch your energy for the last time. </h1>
+                <h1 className="mainBannerBlurb" > Zero hassle automatic switching. Launching soon.</h1>
+                <Email></Email>
+                <p className="mainBannerDisclaimer" > By clicking “Get Notified” you agree to get emails from us upon launch.</p>
+            </div>
         </div>
     )
 }
