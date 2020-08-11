a3cfc67744f19ea44563aad45d066bf1b55c3098
yahm23 <yahmed1996a@gmail.com>
2020-08-11 14:38:58 +0100

    added fading animatun to both success outcomes

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import Email from '../structural/email'
 import Logo from '../media/logoLight.png'
 import Loading from '../structural/loading';
+import shortid from 'shortid';
 
 
 export default function Main() {
@@ -9,6 +10,10 @@ export default function Main() {
     const[success, setSuccess] = useState(false)
     const[processing, setProcessing] = useState(false)
 
+    let getRandomKey = () => {
+        return shortid.generate();
+    }
+    
     return (
         <div className="outerMain">
                 <div className = "mainBanner">
@@ -17,7 +22,10 @@ export default function Main() {
                         :
                         <div>
                             {success?
-                                <div>
+                                <div
+                                    key={getRandomKey()}
+                                    className="fadingAnim">
+                                        
                                     <img className = "logoMain" src={Logo}/>
                                     <h1 className="successText"> Thanks for your interest!</h1>
                                     <h1 className="successText"> We'll get back to you shortly!</h1>
