0ce561a3bcad6998da6f58f6273005dbb5d1367b
yahm23 <yahmed1996a@gmail.com>
2020-08-11 13:21:44 +0100

    chnaged time delay, deleted empty folders

--- src/structural/email.js
+++ src/structural/email.js
@@ -43,7 +43,7 @@ export default function Email(props) {
                     props.setProcessing(false)
                     props.setSuccess(true)
                     
-                  }, 1000);
+                  }, 500);
             }
             else{
                 props.setSuccess(false)
