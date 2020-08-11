06e24cca4ed7f2d9fc9dfd1be1ec4505d953a96e
yahm23 <yahmed1996a@gmail.com>
2020-08-11 11:13:20 +0100

    Revert "changed the animation on second part"
    This reverts commit 4de912d3942eeb0d04d7b890554ad8a7498d0a5e.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,9 @@
 import React, {useState} from 'react'
 import Email from '../structural/email'
 import Logo from '../media/logoLight.png'
-import shortid from "shortid";
 
 
 export default function Main() {
-
-
-    let getRandomKey = () => {
-        return shortid.generate();
-    }
 
     const[success, setSuccess] = useState(false)
 
@@ -17,29 +11,25 @@ export default function Main() {
         <div className="outerMain">
                 <div className = "mainBanner">
                     
-                    {success?
-                        <div
-                        className="fadingAnim"
-                        key={getRandomKey()}
-                        
-                        >
-                            <h1 className="successText"> Thanks for your interest!</h1>
-                            <h1 className="successText"> We'll get back to you shortly!</h1>
-                        </div>
-                        :
-                        
-                        
-                        
+                {success?
+                    <div>
+                        <img className = "logoMain" src={Logo}/>
+                        <h1 className="successText"> Thanks for your interest!</h1>
+                        <h1 className="successText"> We'll get back to you shortly!</h1>
+                    </div>
+                    :
+                    <div>
+                        <img className = "logoMain" src={Logo}/>
+
                         <div className="fadingAnim">
 
-                                <img className = "logoMain" src={Logo}/>
-                                <h1 className="mainBannerText" > Switch your energy for the last time. </h1>
-                                <h1 className="mainBannerBlurb" > Zero hassle automatic switching. Launching soon.</h1>
-                                <Email setSuccess={setSuccess}></Email>
-                                <p className="mainBannerDisclaimer" > By clicking “Get Notified” you agree to get emails from us upon launch.</p>
-                            </div>
-                        
-                    }
+                            <h1 className="mainBannerText" > Switch your energy for the last time. </h1>
+                            <h1 className="mainBannerBlurb" > Zero hassle automatic switching. Launching soon.</h1>
+                            <Email setSuccess={setSuccess}></Email>
+                            <p className="mainBannerDisclaimer" > By clicking “Get Notified” you agree to get emails from us upon launch.</p>
+                        </div>
+                    </div>
+                }
                 </div>
         </div>
     )
